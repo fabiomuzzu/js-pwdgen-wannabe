1b2b6cd09032ec4ae1f2e05309ba6ac2b0d544ee
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-17 13:42:56 +0100

    starting script.js

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let name = prompt("Please enter your name", " ");
+console.log(name);
+
+let surname = prompt("Please enter your surname", " ");
+console.log(surname);
+
+let favouritecolour = prompt("Please enter your favourite color", " ");
+console.log(favouritecolour);
+
