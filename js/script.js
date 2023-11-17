1025e4b7a8b5527beda68c12e8f971a9964fe561
fabiomuzzu <fabio.muzzu@virgilio.it>
2023-11-17 15:54:12 +0100

    final version of the script

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
+// Variabile Name
 let name = prompt("Please enter your name", " ");
 console.log(name);
 
+// Variabile Surname
 let surname = prompt("Please enter your surname", " ");
 console.log(surname);
 
+// Variabile Favouritecolour
 let favouritecolour = prompt("Please enter your favourite color", " ");
 console.log(favouritecolour);
 
+// Variabile Password
+let password = name + surname + favouritecolour + '23'
+console.log(password)
+
+// Variabile per display password in html
+document.getElementById('password').innerHTML = password
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 JS PASSWORD GENERATOR:
 
-1 - Chiedi all’utente il suo nome,
-2 - Poi chiedi il suo cognome,
+1 - Chiedi all’utente il suo nome
+2 - Poi chiedi il suo cognome
 3 - Poi chiedi il suo colore preferito
 4 - Infine scrivi sulla pagina nomecognomecolorepreferito23
